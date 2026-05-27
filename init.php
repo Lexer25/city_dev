da1c95ce4338c10e0a47cb74b3d3b3b85807fb26
Андрей <andrey.buharov@gmail.com>
2026-05-28 00:25:41 +0300

    Автокоммит изменений в dev

--- init.php
+++ init.php
@@ -1,2 +1,20 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 defined('DEV_VERSION') OR define('DEV_VERSION', '2.1.2');
+
+Kohana::$config->load('menu')
+    ->set('dev', array(
+        'title' => 'Очередь загрузки',
+        'url' => 'dev/load_order',
+        'icon' => 'fa-cog',
+        'order' => 10,
+       
+    ));
+	
+Kohana::$config->load('menu')
+    ->set('device', array(
+        'title' => 'Контроллеры',
+        'url' => 'dev/load',
+        'icon' => 'fa-cog',
+        'order' => 9,
+       
+    ));
